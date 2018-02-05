d812bbcbc66da921a527b1e86a9a0ad0bbbc58c3
mdblr <maxodobler@gmail.com>
2018-02-05 13:33:13 -0800

    refactor / incorporate error cases

--- src/public/client.js
+++ src/public/client.js
@@ -11,13 +11,23 @@
     e.preventDefault();
     const term = document.getElementById('search-term').value;
     document.getElementById('search-term').value = '';
-    tableDataFetchMatches(term);
+
+    if (term.trim()) {
+      tableDataFetchMatches(term);
+    } else {
+      displayError('Input cannot be empty.');
+    }
   }
 
   function tableDataFetch(url) {
     fetch(url)
-    .then(res => res.json().then(setTable))
-    .catch(err => console.log(err));
+    .then(res => {
+      if (res.status > 399) {
+        throw new Error('There was a problem on the server.');
+      }
+      res.json().then(setTable)
+    })
+    .catch((err) => displayError(err));
   }
 
   function tableDataFetchAll() {
@@ -30,23 +40,39 @@
 
   function setTable(data) {
     const main = document.querySelector('main');
-    const table = document.querySelector('table') || document.createElement('table');
+    const table = clearTable();
+
+    const tableNodes = (
+      data.names.length > 0
+      ? data.names.map(name => createDataNode(name))
+      : [ createDataNode('No results. Try another name.') ]
+    );
+
+    tableNodes.forEach(node => table.appendChild(node));
+    main.appendChild(table);
+  }
+
+  function displayError(error) {
+    const table = clearTable();
+    const errorNode = createDataNode(error);
+    table.appendChild(errorNode);
+  }
+
+  function clearTable() {
+    const table = (
+      document.querySelector('table') || document.createElement('table'));
 
     if (table.children.length > 0) {
       while (table.lastChild) {
         table.removeChild(table.lastChild);
       }
     }
-
-    const nameNodes = data.names.map(name => createNameNode(name));
-    nameNodes.forEach(node => table.appendChild(node));
-    main.appendChild(table);
+    return table;
   }
 
-  function createNameNode(name) {
+  function createDataNode(name) {
     const tr = document.createElement('tr');
     const td = document.createElement('td');
-    tr.classList.add('name-node');
     tr.appendChild(td);
     td.textContent = name;
     return tr;
